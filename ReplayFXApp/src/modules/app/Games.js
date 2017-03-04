d8e5ed0cba49f69972b238a6edc2030a3e17b60a
Cray J Thomas Jr <cjthoma1@gmail.com>
2017-03-04 11:05:54 -0500

    Updated buttons, styled accordion, added code for modal but no data yet

--- ReplayFXApp/src/modules/app/Games.js
+++ ReplayFXApp/src/modules/app/Games.js
@@ -1,47 +1,77 @@
 import React, {Component} from 'react';
+import {TouchableHighlight, Modal} from 'react-native';
 import {View, Text} from 'react-native-animatable';
 import Accordion from 'react-native-collapsible/Accordion';
 import styles, {stylechoice} from './StyleSheet';
-
-const SECTIONS = [
-  {
-    title: 'Arcade',
-    content: ['Streets of Rage', 'Arkanoid', 'Pac-Man'],
-  },
-  {
-    title: 'Pinball',
-    content: ['Walking Dead', 'Monopoly', 'A something or another'],
-  }
-];
-
+import CloseIcon from '../utils/closeIcon';
 
 export default class Games extends Component {
   constructor(props){
   super(props);
   this.state = {
-    gameSectionHeader: ['Arcade', 'Pinball'],
-    arcadeGames: ['Streets of Rage', 'Arkanoid', 'Pac-Man'],
-    pinballGames: ['Walking Dead', 'Monopoly', 'A something or another']
+    SECTIONS: [
+      {
+      title: 'Arcade',
+      content: ['Streets of Rage', 'Arkanoid', 'Pac-Man'],
+    },
+    {
+      title: 'Pinball',
+      content: ['Walking Dead', 'Monopoly', 'A something or another'],
+    },
+    {
+      title: 'Console',
+      content: ['Sonic','Super Mario', 'Donkey Kong', 'Mario Kart', 'Super Smash Bros']
+    },
+    {
+      title: 'Board',
+      content: ['Monopoly', 'Clue', 'Battle-Ship', 'Sorry']
+    }
+  ],
+  modalVisible: false,
+  modalTitle: '',
+  modalDescription: '',
+  modalImage: '',
+  modalStartTime: '',
+  modalEndTime: '',
+  modalLocation: ''
   };
 
   this._renderHeader = this._renderHeader.bind(this);
   }
-  _renderHeader(section) {
-    return (
-      <View style={styles.header}>
-        <Text style={styles.headerText}>{section.title}</Text>
-      </View>
-    );
+
+  // componentWillReceiveProps(nextProps) {
+  // const sections = this.state.SECTIONS;
+  //   this.setState({
+  //     
+  //   });
+  // }
+  setModalVisible(visible, title, startTime, endTime, location, extendedDescription, image) {
+    this.setState({
+      modalVisible: visible,
+      modalTitle: title,
+      modalStartTime: startTime,
+      modalEndTime: endTime,
+      modalLocation: location,
+      modalDescription: extendedDescription,
+      modalImage: image});
   }
 
 
+
+  _renderHeader(section) {
+    return (
+      <View animation= 'bounceIn' delay= {400}>
+        <Text style={styles.header}>{section.title}</Text>
+      </View>
+    );
+  }
    _renderContent(section) {
      return (
-       <View style={styles.description}>
+       <View>
          {
-           section.content.map((game, index) => {
+           section.content.sort().map((game, index) => {
              return (
-                <Text key= {index}>{game}</Text>
+                <Text style={styles.title} key={index}>{game}</Text>
              );
            })
 
@@ -52,11 +82,40 @@ export default class Games extends Component {
 
    render() {
      return (
+       <View>
+       {/* <Modal
+         animationType={'slide'}
+         transparent={false}
+         visible={this.state.modalVisible}
+         onRequestClose ={() => {
+           this.setModalVisible(!this.state.modalVisible);
+         }}>
+         <View style= {styles.innerContainer}>
+           <Text style={styles.modaltitle}>{this.state.modalTitle}</Text>
+           <Text style={styles.modaldatetime}>{this.state.modalStartTime} - {this.state.modalEndTime}</Text>
+           <Text style ={styles.modaldatetime}>{this.state.modalLocation}</Text>
+           <ScrollView>
+           <View style ={{alignItems: 'center'}}>
+               <Image source={{uri: this.state.modalImage}} style={styles.modalimage}/>
+          </View>
+           <Text style = {styles.modaldescription}>{this.state.modalDescription}</Text>
+         </ScrollView>
+           <View style ={styles.center}>
+           <TouchableHighlight onPress={() => {
+             this.setModalVisible(!this.state.modalVisible);
+           }}>
+             <View><CloseIcon/></View>
+           </TouchableHighlight>
+         </View>
+         </View>
+       </Modal> */}
+
        <Accordion
-         sections={SECTIONS}
+         sections={this.state.SECTIONS}
          renderHeader={this._renderHeader}
          renderContent={this._renderContent}
        />
+     </View>
      );
    }
 
