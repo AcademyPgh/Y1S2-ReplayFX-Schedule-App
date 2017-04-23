3512cc5cc3ee09e79a74f19afe598f1d87babc0e
Cindy Green <cimigreen@hotmail.com>
2017-04-23 09:10:06 -0400

    Changing name of variable and correcting month.

--- ReplayFXApp/src/app/components/PushController.js
+++ ReplayFXApp/src/app/components/PushController.js
@@ -1,4 +1,4 @@
-import {Component} from 'react';
+import React, {Component} from 'react';
 import {AppState} from 'react-native';
 import PushNotification from 'react-native-push-notification';
 
@@ -6,8 +6,8 @@ export default class PushController extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      seconds: 5,
-      fifteenMinTil: new Date(Date.now)
+      fifteenMinutesUntil: new Date(Date.now),
+      seconds: 45
     };
     this.handleAppStateChange = this.handleAppStateChange.bind(this);
   }
@@ -18,7 +18,7 @@ export default class PushController extends Component {
       onNotification: function(notification) {
         console.log('NOTIFICATION:', notification);
       },
-       popInitialNotification: false,
+       popInitialNotification: true,
     });
 
   }
@@ -31,15 +31,14 @@ export default class PushController extends Component {
       if (this.props.item.isFavorite) {
         let _date = new Date(this.props.item.date);
 
-        this.setState({fiftenMinTil: new Date(
-          _date.getFullYear()+"-0"+ _date.getMonth()+"-"+
-          _date.getDate()+"T"+this.props.item.startTime + "-"+"03:45")});
-
-        console.log(this.state.fiftenMinTil);
-
+        this.setState({fifteenMinutesUntil: new Date(
+          _date.getFullYear() +"-0"+ (_date.getMonth()+1)+"-"+_date.getDate()+"T"+this.props.item.startTime+ "-"+"03:45"
+          )});
+          console.log("favorite event time is:" + this.state.fifteenMinutesUntil);
+        
         PushNotification.localNotificationSchedule({
-          message: this.props.item.title + ' is about to begin in 15 minutes',
-          date: new Date(Date.now() + (this.state.seconds * 1000))
+          message: this.props.item.title + ' will begin in 15 minutes',
+          date: new Date(Date.now() + (this.state.seconds * 1000)) //this is where the fire date, based on fifteenMinutesUntil will go
         });
       }
     }
@@ -48,3 +47,5 @@ export default class PushController extends Component {
     return null;
   }
 }
+
+//Mon Jun 26 2017 08:45:00 GMT-0400 (EDT)
