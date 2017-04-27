bddbca4717f98edbc3968aa2af2e34481beb097c
Cindy Green <cimigreen@hotmail.com>
2017-04-27 17:18:23 -0400

    Notifications running correctly but only when app is in background.

--- ReplayFXApp/src/app/components/PushController.js
+++ ReplayFXApp/src/app/components/PushController.js
@@ -6,49 +6,52 @@ export default class PushController extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      fifteenMinutesUntil: new Date(Date.now()),
+      fifteenMinutesUntil: new Date(Date.now),
     };
     this.handleNotification = this.handleNotification.bind(this);
   }
 
   componentDidMount() {
-   AppState.addEventListener('change', this.handleNotification);
+  AppState.addEventListener('change', this.handleNotification);
     PushNotification.configure({
       onNotification: function(notification) {
-        // console.log('NOTIFICATION:', notification);      
+         console.log('NOTIFICATION:', notification);      
       },
     });
+    this.handleNotification();
   }
   
-  componentWillUnmount() {
-   AppState.removeEventListener('change', this.handleNotification);
- }
+   componentWillUnmount() {
+  AppState.removeEventListener('change', this.handleNotification);
+   }
 
   handleNotification(appState) {
-   if (appState=="background" || appState=="active") {
+      if (appState=='background') {
       if (this.props.item.isFavorite) {
-        let _date = new Date(this.props.item.date);
-        //getting time of favorite events
+        let favoriteDate = new Date(this.props.item.date);
+        
+        //getting date of favorite events
 
-        this.setState({fifteenMinutesUntil: new Date(
-          _date.getFullYear() +"-0"+ (_date.getMonth()+1)+"-"+(_date.getDate()+1)+"T"+this.props.item.startTime+ "-"+"03:45"
+      this.setState({fifteenMinutesUntil: new Date(
+          favoriteDate.getFullYear() +"-0"+ (favoriteDate.getMonth()+1)+"-"+(favoriteDate.getDate()+1)+"T"+this.props.item.startTime+ "-"+"03:45"
           )});
-          //converting date to a 15 minutes before the event happens
-           console.log("Notification will happen at :" + this.state.fifteenMinutesUntil);
-           let adate = new Date(this.state.fifteenMinutesUntil)
-            console.log("Time might be "  + adate.getTime() );
-            //seeing if the time is as it should be (15 minutes before the event)
+         //converting date to a 15 minutes before the event happens
+         console.log("Notification will happen at :" + this.state.fifteenMinutesUntil);
+         let adate = new Date(this.state.fifteenMinutesUntil)
+          console.log("Time might be "  + adate.getTime() );
+         //seeing if the time is as it should be (15 minutes before the event)
 
-        if(this.state.fifteenMinutesUntil.getTime() >= Date.now()){
-          PushNotification.localNotificationSchedule({		      
-          message: this.props.item.title + ' will begin in 15 minutes',
-          date: new Date(this.state.fifteenMinutesUntil), 
-          //setting the push notification to fire for each event at the right time
+        if(this.state.fifteenMinutesUntil >= Date.now()){
+           PushNotification.localNotificationSchedule({		      
+           message: this.props.item.title + ' will begin in 15 minutes',
+           date: new Date(this.state.fifteenMinutesUntil), 
+
+           //setting the push notification to fire for each event at the right time
          
-        });
-        }
-      }
+         });
     }
+   }
+ }
   }
   
   
