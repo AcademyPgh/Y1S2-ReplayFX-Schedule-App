4d92aed10b940bc23cc9e24e5dd7f20b6b6b05e8
Cray J Thomas Jr <cjthoma1@gmail.com>
2017-04-29 17:37:24 -0400

    Push notifications rendering once no matter how many times you close the app

--- ReplayFXApp/src/app/components/PushController.js
+++ ReplayFXApp/src/app/components/PushController.js
@@ -7,10 +7,11 @@ export default class PushController extends Component {
     super(props);
     this.state = {
       fifteenMinutesUntil: new Date(Date.now),
-      inAppFifteen: new Date(Date.now)
+      inAppFifteen: new Date(Date.now),
+      previousNotificationsID: 0
     };
     this.backgroundNotification = this.backgroundNotification.bind(this);
-    this.inAppNotification = this.inAppNotification.bind(this);
+    // this.inAppNotification = this.inAppNotification.bind(this);
   }
 
   componentDidMount() {
@@ -18,23 +19,22 @@ export default class PushController extends Component {
     PushNotification.configure({
       onNotification: function(notification) {
          console.log('NOTIFICATION:', notification);
-
-      },
+      }
     });
     // for notifications in the background
 
-    AppState.addEventListener('change', this.inAppNotification);
-    PushNotification.configure({
-      onNotification: function(notification) {
-         console.log('In APP NOTIFICATION:', notification);
-      },
-    });
+    // AppState.addEventListener('change', this.inAppNotification);
+    // PushNotification.configure({
+    //   onNotification: function(notification) {
+    //      console.log('In APP NOTIFICATION:', notification);
+    //   }
+    // });
   }
   // for notifications in App
 
    componentWillUnmount() {
   AppState.removeEventListener('change', this.backgroundNotification);
-  AppState.removeEventListener('change', this.inAppNotification);
+  // AppState.removeEventListener('change', this.inAppNotification);
    }
 
 
@@ -43,29 +43,38 @@ export default class PushController extends Component {
       if (this.props.item.isFavorite) {
         let favoriteDate = new Date(this.props.item.date);
         let id = (this.props.item.id).toString();
+        let previous_notification = this.state.previousNotificationsID;
+        let fifteen_min_until = this.state.fifteenMinutesUntil;
         //getting date of favorite events
-        if (Platform.OS == 'android') {
-      this.setState({fifteenMinutesUntil: new Date(
-          favoriteDate.getFullYear() +"-0"+ (favoriteDate.getMonth()+1)+"-"+(favoriteDate.getDate()+1)+"T"+this.props.item.startTime+ "-"+"03:45"
-          )});
-         //converting date to a 15 minutes before the event happens
-        }
-        else if (Platform.OS == 'ios')
-        {this.setState({fifteenMinutesUntil: new Date(
-          favoriteDate.getFullYear() +"-0"+ (favoriteDate.getMonth()+1)+"-"+(favoriteDate.getDate()+1)+"T"+this.props.item.startTime+ "-"+"03:45"
-          )});}
+        if(previous_notification!= id){
 
-          console.log(this.state.fifteenMinutesUntil);
-        if(this.state.fifteenMinutesUntil >= Date.now()){
+          if (Platform.OS == 'android') {
+            fifteen_min_until = new Date( favoriteDate.getFullYear() +"-0"+ (favoriteDate.getMonth()+1)+"-"+(favoriteDate.getDate()+1)+"T"+this.props.item.startTime+ "-"+"03:45");
+         //converting date to a 15 minutes before the event happens
+          }
+
+          else if (Platform.OS == 'ios')
+          {
+            fifteen_min_until = new Date( favoriteDate.getFullYear() +"-0"+ (favoriteDate.getMonth()+1)+"-"+(favoriteDate.getDate()+1)+"T"+this.props.item.startTime+ "-"+"03:45");
+          }
+
+            console.log(fifteen_min_until);
+            previous_notification = id;
+
+        if(fifteen_min_until >= Date.now()){
            PushNotification.localNotificationSchedule({
             id: id,
             message: this.props.item.title + ' will begin in 15 minutes',
-            date: new Date(this.state.fifteenMinutesUntil),
-
+            date: new Date(fifteen_min_until),
             //sending an id so notification is only sent once
            //setting the push notification to fire for each event at the right time
          });
     }
+  }
+  else{
+    console.log("Previous Notificatons: " + previous_notification);
+  }
+  this.setState({fifteenMinutesUntil: fifteen_min_until, previousNotificationsID: previous_notification});
    }
  }
   }
