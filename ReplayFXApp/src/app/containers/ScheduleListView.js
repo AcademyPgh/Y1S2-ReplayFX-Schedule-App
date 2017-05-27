c647695b76d0a866bedfedd3186a77e93b51f7b6
Cindy Green <cimigreen@hotmail.com>
2017-05-27 16:15:44 -0400

    trying to enable merging with master

--- ReplayFXApp/src/app/containers/ScheduleListView.js
+++ ReplayFXApp/src/app/containers/ScheduleListView.js
@@ -175,7 +175,10 @@ return timeValue;
             <RefreshControl
               refreshing={this.state.isRefreshing}
               onRefresh={this.onRefresh}
-              progressViewOffset = {-30}
+              progressViewOffset = {-20}
+              title="Refreshing..."
+              tintColor="#3B3D68"
+              titleColor="#3B3D68"
               />}
           styles={styles.container}
           dataSource={this.state.dataSource}
