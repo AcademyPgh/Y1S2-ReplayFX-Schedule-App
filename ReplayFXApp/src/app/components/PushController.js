8be13332e6c1ed7da8d8bdaf95a666f1096dc201
Cray J Thomas Jr <cjthoma1@gmail.com>
2017-06-28 12:25:21 -0400

    Notifications are being rendered on device after time has passed

--- ReplayFXApp/src/app/components/PushController.js
+++ ReplayFXApp/src/app/components/PushController.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import{AppState} from 'react-native';
 import PushNotification from 'react-native-push-notification';
 
 export default class PushController extends Component {
@@ -27,7 +28,7 @@ export default class PushController extends Component {
   // }
 
   componentDidMount() {
-  AppState.addEventListener('change', this.backgroundNotification);
+    AppState.addEventListener('change', this.backgroundNotification);
     PushNotification.configure({
       onRegister: function(token) {
         console.log( 'TOKEN:', token );
@@ -40,51 +41,51 @@ export default class PushController extends Component {
   }
 
    componentWillUnmount() {
-  AppState.removeEventListener('change', this.backgroundNotification);
+     AppState.removeEventListener('change', this.backgroundNotification);
    }
 
 
   backgroundNotification(appState) {
-      // if (appState=='background') {
+        if (appState=='background') {
         let favoriteDate = new Date(this.props.item.date);
         let favoriteMonth = '';
         let favoriteDay = '';
         let id = (this.props.item.id).toString();
         let previous_notification = this.state.previousNotificationsID;
-        let fifteen_min_until = this.state.fifteenMinutesUntil;
+        //let fifteen_min_until = this.state.fifteenMinutesUntil;
 
         if (this.props.item.isFavorite) {
         //getting date of favorite events
 
-          if(previous_notification != id){
+          // if(previous_notification != id){
               favoriteMonth  = (favoriteDate.getMonth()+1) >=10 ? "-"+(favoriteDate.getMonth()+1) : "-0"+(favoriteDate.getMonth()+1);
-              favoriteDay  = (favoriteDate.getDate()) >=10 ? "-"+(favoriteDate.getDate()+1) : "-0"+(favoriteDate.getDate()+1);
+              favoriteDay  = (favoriteDate.getDate()) >=10 ? "-"+(favoriteDate.getDate()+1) : "-0"+(favoriteDate.getDate());
               //converting date to a 15 minutes before the event happens
-              fifteen_min_until = new Date( favoriteDate.getFullYear()+favoriteMonth+favoriteDay+"T"+this.props.item.startTime+ "-"+"03:45");
+              let fifteen_min_until = new Date( favoriteDate.getFullYear()+favoriteMonth+favoriteDay+"T"+this.props.item.startTime+ "-"+"03:45");
 
               previous_notification = id;
 
-          if(fifteen_min_until >= Date.now()){
-            console.log(fifteen_min_until);
+          // if(fifteen_min_until >= Date.now()){
+             console.log(fifteen_min_until);
              PushNotification.localNotificationSchedule({
-             date: new Date(fifteen_min_until),
+             date: fifteen_min_until,
              userInfo: {id: id},
              message: this.props.item.title + ' will begin in 15 minutes',
-             id: id
+            id: id
 
             //sending an id so notification is only sent once
            //setting the push notification to fire for each event at the right time
          });
-    }
-  }
-   }
+     }
+   //}
+   //}
       else if(!this.props.item.isFavorite && id == previous_notification){
               PushNotification.cancelLocalNotifications({id: id});
               previous_notification = -1;
    }
-   this.setState({fifteenMinutesUntil: fifteen_min_until, previousNotificationsID: previous_notification});
+    this.setState({fifteenMinutesUntil: fifteen_min_until, previousNotificationsID: previous_notification});
 
- // }
+  }
   }
   render() {
     return null;
--- ReplayFXApp/src/app/containers/ScheduleListView.js
+++ ReplayFXApp/src/app/containers/ScheduleListView.js
@@ -73,9 +73,9 @@ onRefresh() {
             this.props.removeFavorite(item.id);
           // Alert.alert('Item has been removed from your schedule');
           let id = (item.id).toString();
-           PushNotification.cancelLocalNotifications({
-             id: id
-          });
+          //  PushNotification.cancelLocalNotifications({
+          //    id: id
+          // });
         }
 
           else {
@@ -83,21 +83,21 @@ onRefresh() {
             if(this.props.favorites.length < 1){
                  Alert.alert('Item has been added to your schedule');
              }
-           let favoriteDate = new Date(item.date);
-           let favoriteMonth  = (favoriteDate.getMonth()+1) >=10 ? "-"+(favoriteDate.getMonth()+1) : "-0"+(favoriteDate.getMonth()+1);
-         let favoriteDay  = (favoriteDate.getDate()+1) >=10 ? "-"+(favoriteDate.getDate()+1) : "-0"+(favoriteDate.getDate()+1);
-       let fifteenMinutesUntil = new Date (favoriteDate.getFullYear()+favoriteMonth+favoriteDay+"T"+item.startTime+ "-"+"03:45");
-    console.log(fifteenMinutesUntil);
-        
-           let id = (item.id).toString();
-           if( fifteenMinutesUntil >= Date.now()){
-           PushNotification.localNotificationSchedule({
-            id: id,
-             userInfo: {id: id},
-              message: item.title + ' will begin in 15 minutes',
-               date: fifteenMinutesUntil
-           });
-          }
+    //        let favoriteDate = new Date(item.date);
+    //        let favoriteMonth  = (favoriteDate.getMonth()+1) >=10 ? "-"+(favoriteDate.getMonth()+1) : "-0"+(favoriteDate.getMonth()+1);
+    //      let favoriteDay  = (favoriteDate.getDate()+1) >=10 ? "-"+(favoriteDate.getDate()+1) : "-0"+(favoriteDate.getDate()+1);
+    //    let fifteenMinutesUntil = new Date (favoriteDate.getFullYear()+favoriteMonth+favoriteDay+"T"+item.startTime+ "-"+"03:45");
+    // console.log(fifteenMinutesUntil);
+    //
+    //        let id = (item.id).toString();
+    //        if( fifteenMinutesUntil >= Date.now()){
+    //        PushNotification.localNotificationSchedule({
+    //         id: id,
+    //          userInfo: {id: id},
+    //           message: item.title + ' will begin in 15 minutes',
+    //            date: fifteenMinutesUntil
+    //        });
+    //       }
           }
     }
 
@@ -150,7 +150,7 @@ return timeValue;
               this.timeConverter(item.endTime), item.location, item.extendedDescription, item.imageUrl)} //need to redefine the function otherwise tries to change state during render
             onFavoriteButtonPress={this.handleFavoriteButtonPress}
             />
-            <PushController />
+            <PushController item={item}/>
 
     </View>
           )}
