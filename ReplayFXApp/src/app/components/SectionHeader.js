2486b4ffc37ed41f6373d84a6f2cab651cabc122
Cindy Green <cimigreen@hotmail.com>
2017-07-01 12:47:36 -0400

    Added date to header

--- ReplayFXApp/src/app/components/SectionHeader.js
+++ ReplayFXApp/src/app/components/SectionHeader.js
@@ -5,12 +5,15 @@ import styles, {stylechoice} from '../styles/StyleSheet';
 /*Day of week for each of event used as Section Header */
 const SectionHeader = ({sectionData, category}) => {
   let d = new Date(category);
+  let formattedD = ((d.getMonth() + 1) + '/' + (d.getDate() + 1) + '/' +  d.getFullYear());
+
 
   const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   let n = weekday[d.getUTCDay()];
   
+  
   return (<View animation= 'bounceIn' delay= {400}>
-    <Text style={styles.header}>{n}</Text>
+    <Text style={styles.header}>{n} {formattedD}</Text>
   </View>);
 }
 
